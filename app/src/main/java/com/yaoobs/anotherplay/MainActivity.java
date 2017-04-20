fe8aa82bafb4d0ebf54a280d1644e676a4c3d7ba
Yaoobs <yaozheng1990517@163.com>
2017-04-20 12:20:11 +0800

    DrawerLayout

--- app/src/main/java/com/yaoobs/anotherplay/MainActivity.java
+++ app/src/main/java/com/yaoobs/anotherplay/MainActivity.java
@@ -1,13 +1,88 @@
 package com.yaoobs.anotherplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+
+    private View headerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        headerView = mNavigationView.getHeaderView(0);
+
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "headerView clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                switch (item.getItemId()) {
+
+                    case R.id.menu_app_update:
+
+                        Toast.makeText(MainActivity.this, "点击了应用更新", Toast.LENGTH_LONG).show();
+
+                        break;
+
+
+                    case R.id.menu_message:
+
+                        Toast.makeText(MainActivity.this, "点击了消息", Toast.LENGTH_LONG).show();
+
+                        break;
+                }
+
+
+                return false;
+            }
+        });
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
     }
 }
